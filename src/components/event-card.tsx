c9763a5cc02b84280ff3c27e44b907040ba35e67
Kira Zvereva <angryvex@gmail.com>
2024-10-15 20:16:51 +0300

    Events list and card styles

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,9 +1,25 @@
 import { TEventoEvent } from "@/lib/types";
+import Image from "next/image";
 
 type TEventCardProps = {
     event: TEventoEvent;
 };
 
 export default function EventCard({ event }: TEventCardProps) {
-    return <section>{event.name}</section>;
+    return (
+        <section className=" flex flex-col flex-1 h-[380px] basis-80 max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
+            <Image
+                src={event.imageUrl}
+                alt={event.name}
+                width={500}
+                height={280}
+                className="h-[60%] object-fit"
+            />
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <h2 className="text-2xl font-semibold">{event.name}</h2>
+                <p className="italic text-white/75">By {event.organizerName}</p>
+                <p className="text-sm text-white/50 mt-4">{event.location}</p>
+            </div>
+        </section>
+    );
 }
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -7,7 +7,7 @@ type TEventsListProps = {
 
 export default function EventsList({ events }: TEventsListProps) {
     return (
-        <section>
+        <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
             {" "}
             {events.map((event) => (
                 <EventCard key={event.id} event={event} />
