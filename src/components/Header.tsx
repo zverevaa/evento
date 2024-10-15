67ca6ca471b4476d5e1aa8fe8b0dd2288e7eb5b7
Kira Zvereva <angryvex@gmail.com>
2024-10-15 06:01:49 +0300

    Fix header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,33 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
+const routes = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "All Events",
+        path: "/events/all",
+    },
+];
+
 export default function Header() {
     return (
-        <header>
+        <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
             <Logo />
-            <Link href={"/"}>Home</Link>
-            <Link href={"/events/all"}>All Events</Link>
+            <nav>
+                <ul className="flex gap-x-6 text-sm">
+                    {routes.map((route) => (
+                        <li
+                            key={route.name}
+                            className="text-white/50 hover:text-white transition"
+                        >
+                            <Link href={route.path}>{route.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     );
 }
