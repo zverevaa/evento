9888bf4c94c185e93a02f746bf0100fdbbf54b9f
Kira Zvereva <angryvex@gmail.com>
2024-10-15 05:38:04 +0300

    Add logo image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+
 export default function Logo() {
-    return <div>Logo</div>;
+    return (
+        <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+            alt="EVENTO logo"
+            height={12}
+            width={53}
+        />
+    );
 }
