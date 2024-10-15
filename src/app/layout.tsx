7d12d8a38c6069575d168551a4758a1b7811b7a6
Kira Zvereva <angryvex@gmail.com>
2024-10-15 06:37:04 +0300

    Finish home page styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Container from "@/components/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${inter.className} bg-gray-950 text-white`}>
-                <Header />
-                {children}
-                <Footer />
+                <Container>
+                    <Header />
+                    {children}
+                    <Footer />
+                </Container>
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,27 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main>
-            <h1>Find events around you</h1>
-            <p>Browse more than 10,000 events around you</p>
-            <form>
+        <main className="flex flex-col items-center px-3 pt-36">
+            <h1 className="text-3xl lg:text-6xl font-bold tracking-tight ">
+                Find events around you
+            </h1>
+            <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+                Browse more than{" "}
+                <span className="font-bold text-[#a4f839] italic underline">
+                    10,000 events
+                </span>{" "}
+                around you
+            </p>
+            <form className="w-full sm:w-[580px]">
                 <input
+                    className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"
                     placeholder="Search events in any city..."
                     spellCheck={false}
                 />
             </form>
-            <section>
+            <section className="mt-4 flex gap-x-4 text-sm text-white/50">
                 <p>Popular:</p>
-                <div>
+                <div className="space-x-2 font-semibold">
                     <Link href="/events/austin">Austin</Link>
                     <Link href="/events/seattle">Seattle</Link>
                 </div>
