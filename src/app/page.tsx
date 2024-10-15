0238141bf9fbc7329aeebe7ecdb933217271d069
Kira Zvereva <angryvex@gmail.com>
2024-10-15 06:42:46 +0300

    Refactor accent color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
             </h1>
             <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
                 Browse more than{" "}
-                <span className="font-bold text-[#a4f839] italic underline">
+                <span className="font-bold text-accent italic underline">
                     10,000 events
                 </span>{" "}
                 around you
             </p>
             <form className="w-full sm:w-[580px]">
                 <input
-                    className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"
+                    className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
                     placeholder="Search events in any city..."
                     spellCheck={false}
                 />
