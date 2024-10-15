9c969550582ce80b216c3604f3cf9b1737d78a85
Kira Zvereva <angryvex@gmail.com>
2024-10-16 00:32:32 +0300

    Fix images distorting

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -18,7 +18,7 @@ export default function EventCard({ event }: TEventCardProps) {
                     alt={event.name}
                     width={500}
                     height={280}
-                    className="h-[60%] object-fit"
+                    className="h-[60%] object-cover"
                 />
                 <div className="flex flex-col flex-1 justify-center items-center">
                     <h2 className="text-2xl font-semibold">{event.name}</h2>
