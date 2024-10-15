2edffc5a92ffefb1b4cb26a6fcd4a52054f58858
Kira Zvereva <angryvex@gmail.com>
2024-10-15 22:59:12 +0300

    Fetch data for individual events

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,15 @@
-export default function page() {
+type TEventPageProps = {
+    params: {
+        slug: string;
+    };
+};
+
+export default async function EventPage({ params }: TEventPageProps) {
+    const slug = params.slug;
+    const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+    );
+    const event = await response.json();
+
     return <main>page</main>;
 }
