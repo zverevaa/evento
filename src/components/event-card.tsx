6ddad07933825dd3fa6f29469edaf24de9ca7e6a
Kira Zvereva <angryvex@gmail.com>
2024-10-15 20:35:04 +0300

    Event dates

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -7,7 +7,7 @@ type TEventCardProps = {
 
 export default function EventCard({ event }: TEventCardProps) {
     return (
-        <section className=" flex flex-col flex-1 h-[380px] basis-80 max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
+        <section className="relative flex flex-col flex-1 h-[380px] basis-80 max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden">
             <Image
                 src={event.imageUrl}
                 alt={event.name}
@@ -20,6 +20,19 @@ export default function EventCard({ event }: TEventCardProps) {
                 <p className="italic text-white/75">By {event.organizerName}</p>
                 <p className="text-sm text-white/50 mt-4">{event.location}</p>
             </div>
+
+            <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+                <p className="text-xl font-bold -mb-[5px]">
+                    {new Date(event.date).toLocaleDateString("en-US", {
+                        day: "2-digit",
+                    })}
+                </p>
+                <p className="text-xs uppercase text-accent">
+                    {new Date(event.date).toLocaleDateString("en-US", {
+                        month: "short",
+                    })}
+                </p>
+            </section>
         </section>
     );
 }
