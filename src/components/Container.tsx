4cf33f71ce39f5b4742f9769171d55f2e457789d
Kira Zvereva <angryvex@gmail.com>
2024-10-15 06:39:21 +0300

    Type container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function Container({ children }) {
+type TContainerProps = {
+    children: React.ReactNode;
+};
+
+export default function Container({ children }: TContainerProps) {
     return (
         <div className="flex flex-col max-w-7xl min-h-screen mx-auto bg-white/[2%]">
             {children}
