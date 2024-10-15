7413a525574bde0c694b4a31ec5020e94fa3df06
Kira Zvereva <angryvex@gmail.com>
2024-10-15 07:07:08 +0300

    Nav link styles and animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
     {
@@ -13,17 +18,33 @@ const routes = [
 ];
 
 export default function Header() {
+    const activePathname = usePathname();
+
     return (
         <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
             <Logo />
-            <nav>
-                <ul className="flex gap-x-6 text-sm">
+            <nav className="h-full">
+                <ul className="flex gap-x-6 h-full text-sm">
                     {routes.map((route) => (
                         <li
                             key={route.name}
-                            className="text-white/50 hover:text-white transition"
+                            className={clsx(
+                                "hover:text-white flex items-center transition relative",
+                                {
+                                    "text-white": activePathname === route.path,
+                                    "text-white/50":
+                                        activePathname !== route.path,
+                                }
+                            )}
                         >
                             <Link href={route.path}>{route.name}</Link>
+
+                            {activePathname === route.path && (
+                                <motion.div
+                                    layoutId="header-active-link"
+                                    className="bg-accent h-1 w-full absolute bottom-0"
+                                ></motion.div>
+                            )}
                         </li>
                     ))}
                 </ul>
