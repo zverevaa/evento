dfe1fbce62b26cd13aea08e86f7785cb114e4668
Kira Zvereva <angryvex@gmail.com>
2024-10-15 22:50:36 +0300

    Fetch data for correct cities

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -9,12 +9,11 @@ type TEventsPageProps = {
 };
 
 export default async function page({ params }: TEventsPageProps) {
+    const city = params.city;
     const response = await fetch(
-        "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+        `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
     );
     const events: TEventoEvent[] = await response.json();
-
-    const city = params.city;
 
     return (
         <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
