af8a263ad294d30cdfd80dc04ca52507a826ab24
Kira Zvereva <angryvex@gmail.com>
2024-10-15 08:18:03 +0300

    Make h1 dynamic

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,21 @@
 import H1 from "@/components/h1";
 
-export default function page() {
+type TEventsPageProps = {
+    params: {
+        city: string;
+    };
+};
+
+export default function page({ params }: TEventsPageProps) {
+    const city = params.city;
+
     return (
         <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-            <H1>Events in Austin</H1>
+            <H1>
+                {city === "all" && "All Events"}
+                {city !== "all" &&
+                    `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+            </H1>
         </main>
     );
 }
