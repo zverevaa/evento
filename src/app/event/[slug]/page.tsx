40248c27103f6f85cc88e7101f2e26bf14b4c648
Kira Zvereva <angryvex@gmail.com>
2024-10-16 00:31:43 +0300

    Add sections on event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -57,6 +57,32 @@ export default async function EventPage({ params }: TEventPageProps) {
                     </div>
                 </div>
             </section>
+            <div className="min-h-[75vh] text-center px-5 py-16">
+                <Section>
+                    <SectionHeading>About this event</SectionHeading>
+                    <SectionContent>{event.description}</SectionContent>
+                </Section>
+                <Section>
+                    <SectionHeading>Location</SectionHeading>
+                    <SectionContent>{event.location}</SectionContent>
+                </Section>
+            </div>
         </main>
+    );
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+    return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+    return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+    return (
+        <p className="max-w-4xl mx-auto text-lg leading-8 text-white/75">
+            {children}
+        </p>
     );
 }
