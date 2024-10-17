07546984706ca68d1f570eab4d17380d0e5988e6
Kira Zvereva <angryvex@gmail.com>
2024-10-17 20:29:35 +0300

    Change fetching data to the db

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { EventoEvent } from "@prisma/client";
+import prisma from "./db";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -11,17 +11,19 @@ export function capitalize(string: string) {
 }
 
 export async function getEvents(city: string) {
-    const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-    );
-    const events: EventoEvent[] = await response.json();
+    const events = await prisma.eventoEvent.findMany({
+        where: {
+            city: city === "all" ? undefined : capitalize(city),
+        },
+    });
     return events;
 }
 
 export async function getEvent(slug: string) {
-    const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-    );
-    const event: EventoEvent = await response.json();
+    const event = await prisma.eventoEvent.findUnique({
+        where: {
+            slug: slug,
+        },
+    });
     return event;
 }
